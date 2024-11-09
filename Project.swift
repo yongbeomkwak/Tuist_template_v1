b5daf17a55200e12049aa0743e7d7255ae119e28
Hamp <yongbeomkwak@gmail.com>
2024-11-09 17:07:10 +0900

    프로젝트 생성

---
+++ Project.swift
@@ -0,0 +1,31 @@
+import ProjectDescription
+
+let project = Project(
+    name: "TuistTemplateV1",
+    targets: [
+        .target(
+            name: "TuistTemplateV1",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.TuistTemplateV1",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                ]
+            ),
+            sources: ["TuistTemplateV1/Sources/**"],
+            resources: ["TuistTemplateV1/Resources/**"],
+            dependencies: []
+        ),
+        .target(
+            name: "TuistTemplateV1Tests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.TuistTemplateV1Tests",
+            infoPlist: .default,
+            sources: ["TuistTemplateV1/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "TuistTemplateV1")]
+        ),
+    ]
+)
