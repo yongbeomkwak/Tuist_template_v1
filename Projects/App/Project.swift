d332a33b3513765f93291f12ce5f56e17edbce36
Hamp <yongbeomkwak@gmail.com>
2024-11-11 14:20:46 +0900

    워크 스페이스를 통한 프로젝트 위치 변경

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -1,35 +1,35 @@
-//import ProjectDescription
-//import ProjectDescriptionHelpers
-//
-//let project = Project(
-//    name: "TuistTemplateV1",
-//    targets: [
-//        .target(
-//            name: "TuistTemplateV1",
-//            destinations: .iOS,
-//            product: .app,
-//            bundleId: "io.tuist.TuistTemplateV1",
-//            infoPlist: .extendingDefault(
-//                with: [
-//                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
-//                ]
-//            ),
-//            sources: ["Sources/**"],
-//            resources: ["Resources/**"],
-//            scripts: [
-//                .swiftLint
-//             ],
-//            dependencies: []
-//        ),
-//        .target(
-//            name: "TuistTemplateV1Tests",
-//            destinations: .iOS,
-//            product: .unitTests,
-//            bundleId: "io.tuist.TuistTemplateV1Tests",
-//            infoPlist: .default,
-//            sources: ["/Tests/**"],
-//            resources: [],
-//            dependencies: [.target(name: "TuistTemplateV1")]
-//        ),
-//    ]
-//)
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+let project = Project(
+    name: "TuistTemplateV1",
+    targets: [
+        .target(
+            name: "TuistTemplateV1",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.TuistTemplateV1",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                ]
+            ),
+            sources: ["Sources/**"],
+            resources: ["Resources/**"],
+            scripts: [
+                .swiftLint
+             ],
+            dependencies: []
+        ),
+        .target(
+            name: "TuistTemplateV1Tests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.TuistTemplateV1Tests",
+            infoPlist: .default,
+            sources: ["Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "TuistTemplateV1")]
+        ),
+    ]
+)
