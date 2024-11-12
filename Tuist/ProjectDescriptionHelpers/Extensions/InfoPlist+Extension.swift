0d99e3ad8302d54ef905e13e4cd0d01478199e7a
Hamp <yongbeomkwak@gmail.com>
2024-11-12 10:45:19 +0900

    인포 피리스트 확장 실패 임시 마무리

--- Tuist/ProjectDescriptionHelpers/Extensions/InfoPlist+Extension.swift
+++ Tuist/ProjectDescriptionHelpers/Extensions/InfoPlist+Extension.swift
@@ -1,31 +1,45 @@
 import ProjectDescription
 
-//typealias SingleValue = [String : Plist.Value]
-//typealias ArrayValue = [Plist.Value]
+//typealias SingleDictValue = [String : Plist.Value]
+//typealias NestedDictionaryValue = [String:[String: Plist.Value]]
 //
-//extension Dictionary where Key == String, Value == Plist.Value {
+//extension SingleDictValue {
+//    func merging(value: SingleDictValue) -> Self {
+//        var copy = self
+//        return  copy.merging(value, uniquingKeysWith: {value1, _ in return value1 })
+//   }
+//}
+//
+//extension Plist.Value {
+//
+// static func nestDict(_ data: NestedDictionaryValue) -> Plist.Value {
+//    #warning(추후 구현)
+//    }
+//}
+//
+//extension InfoPlist {
 //    enum PlistProperty {
-//        case launchStoryboardName(name: String)
-//        case sceneManifest(array: ArrayValue)
+//        case launchStoryboardName(name: String) //
+//        case sceneManifest(data: ArrayDictionaryValue)
 //        case supportMultipleScenes(flag: Bool)
-//        case sceneConfiguration(array: ArrayValue)
-//        case windowSceneSession(array: ArrayValue)
+//        case sceneConfiguration(data: ArrayDictionaryValue)
+//        case windowSceneSession(data: ArrayDictionaryValue)
 //        case configurationName(name: String)
 //        case delegateClassName
 //        case enableTests(flag: Bool)
 //    
-//        var singleValue: SingleValue {
+//        var singleValue: SingleDictValue {
 //            switch self {
 //            case let .launchStoryboardName(name):
-//                return ["UILaunchStoryboardName" : Plist.Value.string("\(name).storyboard")]
-//            case let .sceneManifest(array):
-//                return ["UIApplicationSceneManifest": Plist.Value.array(array)]
+//                return ["UILaunchStoryboardName" : Plist.Value.string("\(name)")]
+//            case let .sceneManifest(data):
+//                return ["UIApplicationSceneManifest": .arrayDict(data)]
 //            case let .supportMultipleScenes(flag):
 //                return ["UIApplicationSupportsMultipleScenes" : Plist.Value.boolean(flag)]
-//            case let .sceneConfiguration(array):
-//                return ["UISceneConfigurations": Plist.Value.array(array)]
-//            case let .windowSceneSession(array):
-//                return ["UIWindowSceneSessionRoleApplication": Plist.Value.array(array)]
+//            case let .sceneConfiguration(data):
+//                return ["UISceneConfigurations": .arrayDict(data)]
+//            case let .windowSceneSession(data):
+//                return ["UIWindowSceneSessionRoleApplication": .arrayDict(data)]
 //            case let .configurationName(name):
 //                return ["UISceneConfigurationName": Plist.Value.string(name)]
 //            case .delegateClassName:
@@ -37,20 +51,35 @@ import ProjectDescription
 //        }
 //    }
 //    
-//    func merging(property: PlistProperty) -> Self {
-//        var copy = self
-//        return  copy.merging(property.singleValue, uniquingKeysWith: {value1, _ in return value1 })
-//   }
-//    
 //}
 
 extension InfoPlist {
+    
     static var demoDefulat: InfoPlist = .extendingDefault(with: [
         "UIMainStoryboardFile": "",
         "UILaunchStoryboardName": "LaunchScreen",
         "ENABLE_TESTS": .boolean(true),
     ])
     
+//    static var windowSession: SingleDictValue = PlistProperty.windowSceneSession(data: [
+//        InfoPlist.PlistProperty.configurationName(name: "Default Configuration").singleValue,
+//        InfoPlist.PlistProperty.delegateClassName.singleValue
+//    ]).singleValue
+//    
+//    
+//    static var configurations: SingleDictValue = PlistProperty.windowSceneSession(data: [windowSession]).singleValue
+//    
+//    static var manifest: SingleDictValue = PlistProperty.sceneManifest(data: [
+//        PlistProperty.supportMultipleScenes(flag: false).singleValue,
+//        configurations
+//    ]).singleValue
+//    
+//    static var temp: InfoPlist = .extendingDefault(with:
+//        PlistProperty.launchStoryboardName(name: "Resources/LaunchScreen").singleValue
+//        .merging(value: manifest)
+//       
+//    )
+
     static var projectDefault: InfoPlist = .extendingDefault(
         with: [
             "UILaunchStoryboardName": "Resources/LaunchScreen",
